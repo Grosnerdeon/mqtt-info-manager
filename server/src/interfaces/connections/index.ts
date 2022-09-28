789e50c847c92046f1b747f898c7f710fcd26769
Grosnerdeon <59026454+Grosnerdeon@users.noreply.github.com>
2022-09-28 14:40:17 +0300

    Update index.ts

--- server/src/interfaces/connections/index.ts
+++ server/src/interfaces/connections/index.ts
@@ -2,8 +2,8 @@ import { IMQTTMessage } from "../MQTTEntities";
 
 export const REGEXP_URL = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gm;
 export const REGEXP_PORT = /^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/gm;
-export const REGEXP_FILTER_ANY_IN_TOPIC = /\/[+]/gm;
-export const REGEXP_GLOBAL_TOPIC = /#$/gm;
+export const REGEXP_FILTER_ANY_IN_TOPIC = /\/[+]/g;
+export const REGEXP_GLOBAL_TOPIC = /[#]$/g;
 
 export enum connectionAction {
     OPEN_CONNECTION = 'OPEN_CONNECTION',
